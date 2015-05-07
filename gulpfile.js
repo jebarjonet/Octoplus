830028a63ed2ea5ec9be32e634187f4cb172aa47
Jean-Elie Barjonet <jebarjonet@kilix.fr>
2015-05-07 14:54:05 +0200

    Switched project to React/Flux architecture

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,83 @@
 // dependencies
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    jshint = require('gulp-jshint');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var cache = require('gulp-cached');
+var react = require('gulp-react');
+var browserify = require('gulp-browserify');
+var del = require('del');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var plumber = require('gulp-plumber');
 
-var scss_path = 'public/assets/scss/**/*.scss',
-    css_path = 'public/assets/css/',
-    js_path = 'public/assets/js/',
-    angular_files_path = [
-        'public/app/**/*.js',
-        'public/common/**/*.js'
-    ],
-    bower_path = [
-        'bower_components/angular/angular.js', 
-        'bower_components/jquery/dist/jquery.js',
-        'bower_components/angular-ui-router/release/angular-ui-router.js'
-    ],
-    final_path = [
-        'public/assets/js/dependencies.min.js', 
-        'public/assets/js/my.min.js'
-    ];
+var paths = {
+    scss: 'public/assets/scss/**/*.scss',
+    css: 'public/assets/css/',
+    js: 'public/assets/js/',
+    app_js: 'public/app/app.js',
+    watch_js: 'public/app/**/*.js',
+    bower: ['bower_components/jquery/dist/jquery.js']
+};
 
-// Minify
-gulp.task('my_minify', function(){
-    return gulp.src(angular_files_path)
-                .pipe(concat('my.min.js'))
-                .pipe(uglify({mangle: false}))
-                .pipe(gulp.dest(js_path));
-});
-gulp.task('bower_minify', function() {
-    return gulp.src(bower_path)
-                .pipe(concat('dependencies.min.js'))
-                .pipe(uglify({mangle: false}))
-                .pipe(gulp.dest(js_path));
-});
-gulp.task('minify', ['my_minify']);
-// miniyfing bower components only once
-gulp.task('minify_start', ['bower_minify', 'minify']);
-
-// Styles
-gulp.task('sass_compile', function(){
-    return gulp.src(scss_path)
-        .pipe(sass({
-            style: 'compressed'
-        }))
-        .on('error', console.error.bind(console))
-        .pipe(gulp.dest(css_path));
-});
-
-// Copy bootstrap
+// Copy Bootstrap CSS file
 gulp.task('copy', function() {
-    gulp.src('bower_components/bootstrap/dist/css/bootstrap.min.css')
-        .pipe(gulp.dest(css_path));
+    return gulp.src('bower_components/bootstrap/dist/css/bootstrap.min.css')
+        .pipe(plumber())
+        .pipe(gulp.dest(paths.css));
 });
 
-// JSLint
-gulp.task('jslint', function() {
-  return gulp.src(angular_files_path)
-    .pipe(jshint({"bitwise":true,"camelcase":true,"curly":true,"eqeqeq":true,"es3":false,"forin":true,"freeze":true,"immed":true,"indent":4,"latedef":"nofunc","newcap":true,"noarg":true,"noempty":true,"nonbsp":true,"nonew":true,"plusplus":false,"quotmark":"single","undef":true,"unused":false,"strict":false,"maxparams":10,"maxdepth":5,"maxstatements":40,"maxcomplexity":8,"maxlen":120,"asi":false,"boss":false,"debug":false,"eqnull":true,"esnext":false,"evil":false,"expr":false,"funcscope":false,"globalstrict":false,"iterator":false,"lastsemic":false,"laxbreak":false,"laxcomma":false,"loopfunc":true,"maxerr":false,"moz":false,"multistr":false,"notypeof":false,"proto":false,"scripturl":false,"shadow":false,"sub":true,"supernew":false,"validthis":false,"noyield":false,"browser":true,"node":true,"globals":{"angular":false,"$":false}}))
-    .on('error', console.error.bind(console))
-    .pipe(jshint.reporter('default'));
+// Concat and uglify bower dependencies
+gulp.task('bower', function() {
+    return gulp.src(paths.bower)
+        .pipe(plumber())
+        .pipe(concat('dependencies.min.js'))
+        .pipe(uglify({mangle: false}))
+        .pipe(gulp.dest(paths.js));
+});
+
+// JSLint for JSX
+gulp.task('jshint', function() {
+    var stream = gulp.src(paths.watch_js)
+        .pipe(cache('jshint'))
+        .pipe(react())
+        .on('error', function(err) {
+            console.error('JSX ERROR in ' + err.fileName);
+            console.error(err.message);
+            this.end();
+        })
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+    
+    if (process.env.CI) {
+        stream = stream.pipe(jshint.reporter('fail'));
+    }
+    
+    return stream;
+});
+
+// SCSS files compilation
+gulp.task('scss', function(){
+    return gulp.src(paths.scss)
+        .pipe(plumber())
+        .pipe(sass({style: 'compressed'}))
+        .pipe(gulp.dest(paths.css));
+});
+
+// Browserify and compile JSX files
+gulp.task('js', function() {
+    return gulp.src(paths.app_js)
+        .pipe(plumber())
+        .pipe(browserify({transform:'reactify'}))
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('public/'));
 });
 
 // Watch
 gulp.task('watch', function(){
-    gulp.watch(scss_path, ['sass_compile']);
-    gulp.watch(angular_files_path, ['jslint', 'minify']);
+    gulp.watch(paths.scss, ['scss']);
+    gulp.watch(paths.watch_js, ['jshint', 'js']);
 });
 
-gulp.task('default', ['sass_compile', 'jslint', 'minify_start', 'copy', 'watch']);
+gulp.task('default', ['copy', 'bower', 'jshint', 'scss', 'js', 'watch']);
